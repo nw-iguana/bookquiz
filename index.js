f98d80a5e7b0a0a160641ab93ce93e4b0dadc87d
Felix Meyer <phelixm@gmail.com>
2019-02-08 00:33:42 -0600

    added preliminary thoughts and defined data types

--- index.js
+++ index.js
@@ -2,36 +2,57 @@ function stubs
 features
 pseudocode functions
 
-function render() {
-    if (store.view === 'start') {
-      $('.intro').show();
-      $('.quiz').hide();
-      $('.status').hide();
-    } else if (store.view === 'quiz') {
-      $('.intro').hide();
-      $('.quiz').show();
-      $('.status').show();
-    }
-  }
-
   // In-memory database of questions (static questions data)
-const QUESTIONS = [];
+const QUESTIONS = [
+  {
+    number: 1,
+    question: `Who wrote The Three Musketeers?`,
+    firstAnswer: "Victor Hugo",
+    secondAnswer: "Alexandre Dumas",
+    thirdAnswer: "Albert Camus",
+    fourthAnswer: "Marcel Proust"
+    correctAnswer: secondAnswer, // Not sure if this is a good way to set this property.
+  },
+  {
+    number: 2,
+    // etc.
+  }
+];
 
 // Create your initial store (track progress overtime)
 const STORE = {
-    // Current question
-    // User's answer choice(s)
-    // Current view
-    // Score? Anything else?
+    viewIndex: 0, /* Title  will be 0, and viewIndex will increment
+     when the user pushes "Next" button to move to the next question (include that in the appropriate eventHandler function)
+     The question number(AKA QUESTIONS.number) will match the viewIndex. End page will be 11.*/
+    selectedAnswer: "",
+    viewStatus: "start",
+    score: 0,
 };
 
-// Template generators (generates HTML based on data)
+// Template generators (generates HTML based on data) FELIX
 function generateAnswerList(answers) {}
 
-// Rendering functions (Reads STORE, calls generators, then adds HTML to DOM)
-function renderQuestionText() {}
+// Rendering functions (Reads STORE, calls generators, then adds HTML to DOM) FELIX
 
-// Event handlers (Get user input, update STORE, then call renderers)
+function renderQuestionText() {
+  generateAnswerList(STORE.number)
+}
+
+// Need to integrate this function into the rest of the code. Maybe into renderQuestionText?
+function render() {
+  if (store.viewStatus === 'start') {
+    $('.intro').show();
+    $('.quiz').hide();
+    $('.status').hide();
+  } else if (store.viewStatus === 'quiz') {
+    $('.intro').hide();
+    $('.quiz').show();
+    $('.status').show();
+  }
+}
+
+
+// Event handlers (Get user input, update STORE, then call renderers) ANYA
 function handleAnswerSubmitted() {
   $('.user-controls').on('click', '.submit-answer', () => {
     // Retrieve answer identifier of user-checked radio button
@@ -40,7 +61,17 @@ function handleAnswerSubmitted() {
   });
 }
 
-//Initialize event listeners when DOM is ready
+//Initialize event listeners when DOM is ready ANYA
 $(function(){
     handleAnswerSubmitted();
 });
+
+// Main function that calls the renderer and the event handler(s).
+// The order these functions are called in may need to change based on the program logic.
+function main() {
+  renderQuestionText();
+  handleAnswerSubmitted();
+}
+
+// When the page loads, call main
+$(main);
